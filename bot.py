f05c7cd8a5a34ca7d6319630359861c6aba1dac6
galakcy36-eng <galakcy36@gmail.com>
2026-04-26 01:11:25 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+import discord
+import os
 
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print("Bot connecté !")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == "!ping":
+        await message.channel.send("pong 🏓")
+
+client.run(os.environ['TOKEN'])
